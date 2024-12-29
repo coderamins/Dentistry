44c5fae44624572a3b21c752e9afc2f90e71463e
coderamins <raminsolhi1@gmail.com>
2024-12-29 07:27:52 +0330

    445545

---
+++ Dentistry.Infrastructure/Entity/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dentistry.Infrastructure.Entity
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MobileNo { get; set; }
+        public string  CustomerAddress { get; set; }
+    }
+}
---
+++ Dentistry.Infrastructure/Entity/DentistryRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dentistry.Infrastructure.Entity
+{
+    public class DentistryRequest
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public string RequestMessage { get; set; }
+
+        public virtual Customer Customer { get; set; }
+    }
+
+}
