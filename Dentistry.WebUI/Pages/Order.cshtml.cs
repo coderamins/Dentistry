55de2901b583719cfff0888d5572a85e3a285f4a
coderamins <raminsolhi1@gmail.com>
2025-01-15 01:19:56 +0330

    202501150119

---
+++ Dentistry.WebUI/Pages/Order.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Dentistry.WebUI.Pages
+{
+    public class OrderModel : PageModel
+    {
+        private readonly ILogger<OrderModel> _logger;
+
+        public OrderModel(ILogger<OrderModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+
+}
